e21f7167b713435cd9d578e068e9104509b1cfd9
AbrahamAvelar <jabrahamavelar@gmail.com>
2019-03-19 14:18:02 -0600

    Add files via upload

---
+++ CorrelationScatter.m
@@ -0,0 +1,34 @@
+function [plotHandle r p n m] = CorrelationScatter(x,y,labelx, labely, namesx, namesy, textos)
+% [plotHandle r p] = CorrelationScatter(x,y,labelx, labely, namesx, namesy,textos)
+error(nargchk(0,7,nargin))
+if length(x) ~= length(y)
+    warning('X and Y have different sizes');
+end
+
+if nargin>4 %if names are provided, they must be paired in both vectors
+    i=FindGeneList(namesx,namesy);
+    y=y(i);
+end
+
+xvalid = find(~isnan(x));
+yvalid = find(~isnan(y));
+
+validas = intersect(xvalid,yvalid);
+[r p]=corrcoef(x(validas), y(validas));
+r=r(2);
+p=p(2);
+
+plotHandle = plot(x(validas),y(validas),'ok');
+%legend(strcat(' r= ',num2str(r),' p= ',num2str(p)),'location','best')
+titulo=strcat(' r= ',num2str(r),' p= ',num2str(p), '  n=', num2str(length(validas)));
+fastLabels(labelx, labely, titulo);
+
+n=length(validas);
+m=robustfit(x(validas),y(validas));
+if nargin>6
+    text(x(validas)+.001,y(validas),textos(validas))
+end
+grid on
+axis square
+end
+
